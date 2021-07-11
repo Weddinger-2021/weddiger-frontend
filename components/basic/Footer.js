a8c187b36eb77a7036d12c2377e50fd4da15491c
NOOR OMARI <nooromari847@gmail.com>
2021-07-12 02:05:52 +0300

    add logo, head done, header css need fix

---
+++ components/basic/Footer.js
@@ -0,0 +1,9 @@
+
+
+export default function Footer(){
+    return(
+        <footer className="flex items-center justify-center w-full h-24 border-t mb-0">
+        <p> Weddinger &copy; 2021-PyPandas </p>
+      </footer>
+    )
+}
