cd398995840448fafbc6e613b9bb6ece05392023
NOOR OMARI <nooromari847@gmail.com>
2021-07-11 20:00:52 +0300

    fix image path error

--- components/category.js
+++ components/category.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Category (props){
     return (
         <div>
-        <Image src="/img/wedding-venues-feature.png" alt="Picture of category" height="200px" width="200px" />            
+        <Image src="/img/Floral-filled-Indoor-Garden-Wedding-Inspiration-Cristy-Angulo-Photography-21.png" alt="Picture of category" height="200px" width="200px" />            
         <h2>Venues</h2>
         <Link href='client/Services'>
         <a className="bg-gray-100 text-gray-800 rounded-lg mr-4 pb-1 pl-1 pr-1 text-l float-right " >Services</a>
