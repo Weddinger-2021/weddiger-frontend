025efa86f8bbc8987111f5b2c12c4e4ab40d1dc4
NOOR OMARI <nooromari847@gmail.com>
2021-07-11 21:12:11 +0300

    fix cetegories render error

--- components/category.js
+++ components/category.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Category ({data}){
     return (
         <div>
-          {data.map((category,i) => {
+          {data.map((category,i) =>
           <section key={i} >
             <h2>{category}</h2>
             <Image src="/img/Fitroy+wedding+venue.jpg" alt="Picture of category" height="200px" width="200px" />            
@@ -13,7 +13,7 @@ export default function Category ({data}){
             <a className="bg-gray-100 text-gray-800 rounded-lg mr-4 pb-1 pl-1 pr-1 text-l float-right " >Services</a>
             </Link>        
         </section>
-        })}
+      )}
       </div>
     )
 }
