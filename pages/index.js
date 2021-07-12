a5333640951fb95e930ab619aaf3b6585b846029
Muhannad <Muhannadalmughrabi233@gmail.com>
2021-07-12 16:22:07 +0300

    merge basic components with add services form

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
+import Link from 'next/link'
 import WeddHead from '../components/basic/WeddHead'
 import Header from '../components/basic/Header'
 import Footer from '../components/basic/Footer'
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen justify-between" >
+    <div className="flex flex-col justify-between h-screen" >
       <WeddHead title ="Landing Page" />
       < Header />
+      {/* this link should be in service provider dashboard */}
+      <button><Link href="/serviceProvider/ServiceAdd">Service Add Form</Link></button>
       < Footer />
     </div>
   )
