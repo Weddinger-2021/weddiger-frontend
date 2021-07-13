0374bc036c18fc5da3e4f3ffd18df10136c3fabe
NOOR OMARI <nooromari847@gmail.com>
2021-07-14 00:42:03 +0300

    fix components path

--- pages/client/ServiceDetails.js
+++ pages/client/ServiceDetails.js
@@ -1,7 +1,7 @@
 
 
 import React from "react";
-import Service from "../../components/service";
+import Service from "../../components/serviceComp";
 import WeddHead from '../../components/basic/WeddHead'
 import Header from '../../components/basic/Header'
 import Footer from '../../components/basic/Footer'
