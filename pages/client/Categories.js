bb782659c72c597c04b6ade8841cd5567e29eeef
NOOR OMARI <nooromari847@gmail.com>
2021-07-13 20:27:54 +0300

    login and fetchData from backend API

--- pages/client/Categories.js
+++ pages/client/Categories.js
@@ -1,16 +1,36 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import Category from "../../components/category";
 import {categories} from '../../data'
 import WeddHead from '../../components/basic/WeddHead'
 import Header from '../../components/basic/Header'
 import Footer from '../../components/basic/Footer'
 import Login from "../../components/Login";
+import { fetchData } from "../api/api_get_post";
 
 export default function category_group() {
-    const [isLogin, setIsLogin] = useState(false)
+    const [categories, setCategories] = useState([]);
+    const [isLogin, setIsLogin] = useState(false);
+    
+    const [token, setToken] = useState('');
+    
+    // useEffect(() => {
+    //     localStorage.setItem('accessToken', token);
+    // }, [token]);
+
     function handelSubmit(e){
+        e.preventDefault();
+        const loginInfo = {
+            email : e.target.email.value ,
+            password : e.target.password.value,
+          };
+          getData(loginInfo)
         setIsLogin(true)
       }
+
+     async function getData(loginInfo) {
+        setCategories(await fetchData(loginInfo,'categories'));
+    }
+
     if (isLogin){
     return (
         <div className="flex flex-col h-screen justify-between" >
