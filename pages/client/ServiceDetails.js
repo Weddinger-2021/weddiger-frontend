21ffeed99f851c617c54d6ec050dcce4fc601751
NOOR OMARI <nooromari847@gmail.com>
2021-07-14 13:56:32 +0300

    fix error for deplyment

--- pages/client/ServiceDetails.js
+++ pages/client/ServiceDetails.js
@@ -1,21 +1,22 @@
 
 
-import React from "react";
-import Service from "../../components/serviceComp";
-import WeddHead from '../../components/basic/WeddHead'
-import Header from '../../components/basic/Header'
-import Footer from '../../components/basic/Footer'
-import {services_venues} from '../../data'
-import ServiceDetails from '../../components/serviceDetailcomp'
+// import React from "react";
+// import Service from "../../components/serviceComp";
+// import WeddHead from '../../components/basic/WeddHead'
+// import Header from '../../components/basic/Header'
+// import Footer from '../../components/basic/Footer'
+// import {services_venues} from '../../data'
+// import ServiceDetails from '../../components/serviceDetailcomp'
 
 export default () => {
 
   return (
     <div className="flex flex-col justify-between h-screen">
-        <WeddHead title="Details" />
+      <h1>hellooo</h1>
+        {/* <WeddHead title="Details" />
         < Header />
         <ServiceDetails services_venues={services_venues} />
-        < Footer />
+        < Footer /> */}
     </div>
     
   );
